2ef6ef203947dee7cdd83074e744c90b3d1bdd6f
lixiaochunmac <490398311@qq.com>
2018-03-20 07:16:33 +0800

    add 图库

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,10 +10,25 @@
     active-text-color="#ffd04b">
     <el-menu-item index="/">safety</el-menu-item>
     <el-menu-item index="/list">探索</el-menu-item>
-    <li class="ask">
-      <el-button type="primary" icon="el-icon-edit" @click="handleClick">提问</el-button>
+    <li class="search">
+      <el-input placeholder="请输入内容" v-model="search" class="input-with-select">
+        <el-button slot="append" icon="el-icon-search"></el-button>
+      </el-input>
     </li>
-    <li class="ask"><el-button type="primary" icon="el-icon-search">搜索</el-button></li>
+    <li class="ask">
+      <el-button type="primary" icon="el-icon-upload" @click="dialogVisible = true">库</el-button>
+      <el-dialog
+        title="提示"
+        :visible.sync="dialogVisible"
+        width="30%"
+        :before-close="handleClose">
+        <span>这是一段信息</span>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        </span>
+      </el-dialog>
+    </li>
     <el-menu-item index="/signup" class="sign">注册</el-menu-item>
     <el-menu-item index="/signin" class="sign">登录</el-menu-item>
   </el-menu>
@@ -25,6 +40,8 @@
     data() {
       return {
         active: '/',
+        search: null,
+        dialogVisible: false
       };
     },
     methods: {
@@ -35,7 +52,7 @@
         console.log('click');
         const h = this.$createElement;
         this.$msgbox({
-          title: '遇到的问题',
+          title: '图库',
           message: h('p', null, [
             h('span', null, '内容可以是 '),
             h('i', { style: 'color: teal' }, 'VNode')
@@ -63,6 +80,13 @@
             message: 'action: ' + action
           });
         });
+      },
+      handleClose(done) {
+        this.$confirm('确认关闭？')
+          .then(_ => {
+            done();
+          })
+          .catch(_ => {});
       }
     }
   }
@@ -72,7 +96,10 @@
   .sign{
     float: right;
   }
-  .ask {
+  .search {
+    width: 300px;
+  }
+  .ask, .search {
     float: left;
     height: 60px;
     line-height: 60px;
